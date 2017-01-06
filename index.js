0b60ad7aa07093f4cdc7cee862781fffb60009ee
Justin Zelinsky <jpzelinsky@gmail.com>
2017-01-06 10:21:12 -0500

    refactoring

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const request = require('request');
 
 const apiKey = "NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo"; // Probably should get my own key
 
-const start = new Date(1995, 5, 16); // First APOD
-const end = new Date();
-const randomDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+const start = new Date(1995, 5, 16).getTime(); // First APOD
+const end = new Date().getTime();
+const randomDate = new Date(start + Math.random() * (end - start)).toISOString().slice(0, 10);
+const apodUrl = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey + "&hd=true&date=" + randomDate;
 
-const apodUrl = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey + "&hd=true&date=" + randomDate.toISOString().slice(0, 10);
-
-request(apodUrl, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const hdUrl = JSON.parse(body).hdurl;
-    const fileName = hdUrl.slice(hdUrl.lastIndexOf('/') + 1);
-    request(hdUrl).pipe(fs.createWriteStream('/tmp/' + fileName)).on('finish', function() {
-      osascript.execute('tell application "Finder" to set desktop picture to POSIX file "/tmp/' + fileName + '"');
-    });
-  }
+request(apodUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        const {hdurl} = JSON.parse(body);
+        const fileName = hdurl.slice(hdurl.lastIndexOf('/') + 1);
+        request(hdurl)
+            .pipe(fs.createWriteStream('/tmp/' + fileName))
+            .on('finish', () => osascript.execute('tell application "Finder" to set desktop picture to POSIX file "/tmp/' + fileName + '"'));
+    }
 });
